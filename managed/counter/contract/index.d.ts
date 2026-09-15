3c710986f73fc20abd212e527d9b19b5428bb906
Samuel1505 <sok.akinola@gmail.com>
2026-09-15 15:24:36 +0100

    Compile counter contract (managed/ zkir, keys, and JS artifacts)

---
+++ managed/counter/contract/index.d.ts
@@ -0,0 +1,47 @@
+import type * as __compactRuntime from '@midnight-ntwrk/compact-runtime';
+
+export type Witnesses<PS> = {
+  localSecretPin(context: __compactRuntime.WitnessContext<Ledger, PS>): [PS, Uint8Array];
+}
+
+export type ImpureCircuits<PS> = {
+  increment(context: __compactRuntime.CircuitContext<PS>): __compactRuntime.CircuitResults<PS, []>;
+  setPin(context: __compactRuntime.CircuitContext<PS>, newPinHash_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type ProvableCircuits<PS> = {
+  increment(context: __compactRuntime.CircuitContext<PS>): __compactRuntime.CircuitResults<PS, []>;
+  setPin(context: __compactRuntime.CircuitContext<PS>, newPinHash_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type PureCircuits = {
+  hashPin(pin_0: Uint8Array): Uint8Array;
+}
+
+export type Circuits<PS> = {
+  hashPin(context: __compactRuntime.CircuitContext<PS>, pin_0: Uint8Array): __compactRuntime.CircuitResults<PS, Uint8Array>;
+  increment(context: __compactRuntime.CircuitContext<PS>): __compactRuntime.CircuitResults<PS, []>;
+  setPin(context: __compactRuntime.CircuitContext<PS>, newPinHash_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type Ledger = {
+  readonly round: bigint;
+  readonly pinHash: Uint8Array;
+}
+
+export type ContractReferenceLocations = any;
+
+export declare const contractReferenceLocations : ContractReferenceLocations;
+
+export declare class Contract<PS = any, W extends Witnesses<PS> = Witnesses<PS>> {
+  witnesses: W;
+  circuits: Circuits<PS>;
+  impureCircuits: ImpureCircuits<PS>;
+  provableCircuits: ProvableCircuits<PS>;
+  constructor(witnesses: W);
+  initialState(context: __compactRuntime.ConstructorContext<PS>,
+               initialPinHash_0: Uint8Array): __compactRuntime.ConstructorResult<PS>;
+}
+
+export declare function ledger(state: __compactRuntime.StateValue | __compactRuntime.ChargedState): Ledger;
+export declare const pureCircuits: PureCircuits;
